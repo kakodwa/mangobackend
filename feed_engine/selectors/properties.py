d96924038ef1b73d0258a22b55c60e631e9fb040
ikpixels <ikpixels.py@gmail.com>
2026-06-04 09:22:59 +0200

    profeed

--- feed_engine/selectors/properties.py
+++ feed_engine/selectors/properties.py
@@ -1,4 +1,5 @@
 from realestate.models import Property
 
 def get_properties():
-    return Property.objects.filter(is_publicly_visible=False).order_by('-id')
+    #return Property.objects.filter(is_publicly_visible=False).order_by('-id')
+    return Property.objects.all().order_by('-id')
