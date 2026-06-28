f7be760bc237aa127cd519f234fd7775ce4df229
Kakodwa <ikpixels.py@gmail.com>
2026-06-28 21:34:30 +0200

    updater

--- static/flutter_bootstrap.js
+++ static/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3774311848" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3879409774" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
